2fd99fcdb5f9faa3813e1d44860cab703de84f4d
Conrad Leonik <Conrad@Conrad.local>
2017-11-19 23:17:38 -0600

    preliminary basin of attraction functional

--- testline.py
+++ testline.py
@@ -17,8 +17,35 @@ period = float(sys.argv[3])
 x = np.linspace(0., 1., len(v[:,0]))
 for i in range(len(x)):
 	x[i] = round(x[i], 3)
-print x
+print x # x is basically phi, v[0], and v[1] is basically f(phi)
 y = (2. * x) - 1. - (2. * (delay / period))	#making line for 2-cluster stability
+print y
+print type(y)
+print v
+print type(v)
+z = x + 2 * delay / period
+print z
+print "testing thing"
+# w is storing basin of attraction, formula f(phi) = f(1 - phi + f(phi + 2 * delay / period))
+w = y.copy()
+for i in range(len(x)):
+	#print round(z[i], 2), v[i][0]
+	for j in range(len(x)):
+		if round(z[i], 2) == v[j][0]:
+			print round(z[i], 2), "adding", v[j][1], "to w"
+			w[i] = v[j][1]
+print w
+# w should now contain f(phi + 2 * delay / period)
+w = 1 - x + w
+print w
+# w should now contain 1 - phi + f(phi  + 2 * delay / period)
+for i in range(len(x)):
+	#print round(z[i], 2), v[i][0]
+	for j in range(len(x)):
+		if round(w[i], 2) == v[j][0]:
+			print round(w[i], 2), "adding", v[j][1], "to w"
+			w[i] = v[j][1]
+# w should now contain f(1 - phi + f(phi + 2 * delay / period))
 sync = round(delay / period, 3)
 print "sync = ", sync
 if sync / 0.002 != 0.0:
@@ -33,6 +60,8 @@ idx = np.argwhere(np.diff(np.sign(y - v[:,1])) != 0).reshape(-1)
 print "best guess at intersection points"
 print idx
 rem = 0
+#now trying to add automatic basin of attraction functionality, this should be fun
+
 #~ while rem >= 0:
 	#~ rem = int(raw_input("If there is a mistake, enter the index of a point to remove it, else enter -1\n"))
 	#~ if rem >= 0:
@@ -42,6 +71,7 @@ rem = 0
 ax1.plot(v[:,0], v[:,1],"k-")
 ax1.plot(x[idx], y[idx], 'ro')
 ax1.plot(sync, v[:,1][syncnum], 'ro')
+ax1.plot(x, w, "g--")
 print "Values where two cluster line intersects with PRC"
 print idx
 
