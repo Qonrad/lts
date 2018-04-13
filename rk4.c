ae61021400afd8adeefb046a06b9d2e378fc63eb
Conrad <Conrad@Conrads-MacBook-Pro.local>
2018-04-13 15:41:33 -0500

    void

--- rk4.c
+++ rk4.c
@@ -74,6 +74,7 @@
  * properly able to cat args.txt into parameters to perfectly recreate exact running of specific commit
  get prc and main to use the same derivs and rk4
  note: maybe its the divnn that's causing the difference from lowhigh to now, also population & mycluster?
+ number of spikes difference with old.c is still sort of mysterious
  * 
  */
 
