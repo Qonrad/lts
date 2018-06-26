48b305dbb056010d0a482148b725e497415c30b6
Conrad <cleoni@lsuhsc.edu>
2018-06-26 14:28:12 -0500

    first attempt

--- gw.py
+++ gw.py
@@ -4,6 +4,7 @@ It first writes weights for all-to-all connected and then optionally changes to 
 '''
 n = 20
 weightstr = ''
+wl = [] #weights list
 for i in range(n):
 	for j in range(n):
 		if i == j:
@@ -12,3 +13,14 @@ for i in range(n):
 			weightstr += '1. '
 	weightstr += '\n'
 print(weightstr)
+def pw(wl): #prints the weights
+	for i in le
+	return
+for i in range(n):
+	for j in range(n):
+		if i == j:
+			wl += ['0. ']
+		else:
+			wl += ['1. ']. '
+	wl += ['\n']
+	#wl should be list of lists, simple fix, but no time now
