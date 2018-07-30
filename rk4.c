53dbad611956b1ed0e93f91d6fcffcd5e7e142d8
Conrad <conradleonik@gmail.com>
2018-07-30 14:51:59 -0500

    added option for onlyprc to avoid wasting computational resources

--- rk4.c
+++ rk4.c
@@ -113,6 +113,7 @@ static char args_doc[] = "INPUTFILE NUMBER_OF_NUERONS";
 /* The options we understand. */
 static struct argp_option options[] = {
 	{"prc",		'p', "MYCLUSTER",	0, "Run PRC. Argument is size of cluster." },
+	{"onlyprc",	'o', "ONLYPRC",		OPTION_ARG_OPTIONAL, "toggles doing only PRC, does nothing if --prc=CLUSTERSIZE not enabled"},
 	{"etime",	'e', "ENDTIME",		0, "ENDTIME of main simulation. Default is 5000."},
 	{"lowprop", 'l', "LOW_RANGE",	0, "Use Low-dose Propofol. Mandatory argument is time range eg: 0-5000"},
 	{"highprop",'h', "HIGH_RANGE",	0, "Use High-dose Propofol. Mandatory arguement is time range eg: 0-5000"},
@@ -144,6 +145,7 @@ struct arguments
   int highend;
   int commit;
   int verbose;
+  int onlyprc;
   double delay;
   double stepsize;
   double iapparg;
@@ -204,6 +206,9 @@ parse_opt (int key, char *arg, struct argp_state *state)
 		case 't':
 			arguments->phase = atof(arg);
 			arguments->trace = 1;
+			break;
+		case 'o':
+			arguments->onlyprc = 1;
 			break;
 		case ARGP_KEY_ARG:
 			if (state->arg_num >= 2)
@@ -337,7 +342,7 @@ void derivs(double time, double *y, double *dydx, double *oldv, double* weight) 
 				dydx[P] = 2 * (1 + tanh(*pert / 4.0)) * (1 - y[P]) - y[P] / tau;
 			}
 			else {
-				dydx[P] = 2 * (1 + tanh((STHRESHOLD) / 4.0)) * (1 - y[P]) - y[P] / tau;	//not sure why THRESHOLD is being used here?
+				dydx[P] = 2 * (1 + tanh((-50.0) / 4.0)) * (1 - y[P]) - y[P] / tau;	//not sure why THRESHOLD is being used here?
 			}
 		}
 		else {
@@ -875,6 +880,7 @@ int main(int argc, char **argv) {
 	arguments.interval = 100;
 	arguments.clustersize = 10;
 	arguments.iapparg = 1.81;
+	arguments.onlyprc = 0;
 
 	/* Parse our arguments; every option seen by parse_opt will
 	be reflected in arguments. */
@@ -903,210 +909,213 @@ int main(int argc, char **argv) {
 	//currently it seems like this nstep is reused for prc initial simulation for creating the template
 	//this hasn't really created a problem yet, and it isn't likely to since that simulation will almost always need to be longer
 	//but it may be important to keep in mind regardless
-	fprintf(stderr, "The initial simulation will contain %d steps.\n", nstep);
 	int i, j, k, pre;
 	double time;
 	double v[N * nn], vout[N * nn], dv[N * nn];					//v = variables (state and current), vout = output variables, dv = derivatives (fstate)
 	double **y, *xx; 						//results variables, y[1..N][1..NSTEP+1], xx[1..NSTEP+1]
 	extern double current[];				//external variable declaration
-	double weight[nn*nn];
-	scan_(weight, nn*nn, "weights.data");
-	
-	fprintf(stderr, "DELAY = %fms.\n", arguments.delay);
-	if (arguments.lowprop) {
-		fprintf(stderr, "Using Low Dose Propofol starting at time %d and ending at %d.\n", arguments.lowstart, arguments.lowend);
-	}
-	if (arguments.highprop) {
-		fprintf(stderr, "Using High Dose Propofol starting at time %d and ending at %d.\n", arguments.highstart, arguments.highend);
-	}
-	/*
-	fprintf(stderr, "Printing data to file ");
-	fprintf(stderr, FULLNAME);
-	fprintf(stderr, "\n");
-	*/
+	if (!arguments.onlyprc) {
+		fprintf(stderr, "The initial main simulation will contain %d steps.\n", nstep);
+		double weight[nn*nn];
+		scan_(weight, nn*nn, "weights.data");
+		
+		fprintf(stderr, "DELAY = %fms.\n", arguments.delay);
+		if (arguments.lowprop) {
+			fprintf(stderr, "Using Low Dose Propofol starting at time %d and ending at %d.\n", arguments.lowstart, arguments.lowend);
+		}
+		if (arguments.highprop) {
+			fprintf(stderr, "Using High Dose Propofol starting at time %d and ending at %d.\n", arguments.highstart, arguments.highend);
+		}
+		/*
+		fprintf(stderr, "Printing data to file ");
+		fprintf(stderr, FULLNAME);
+		fprintf(stderr, "\n");
+		*/
 
-	//Variables to do with delay
-	int dsteps = (int)(arguments.delay / arguments.stepsize);	//number of steps in the delay (i.e. number of elements in the buffer)
-	double* buf[dsteps];
-	for (i = 0; i < dsteps; ++i) {
-		buf[i] = (double*) malloc(sizeof(double) * nn);		//allocates memory for an array of arrays, length depending on the number of neurons in the simulation
-	}
-	int bufpos = 0;								//holds the position in the buffer that the del  pointer is at
-	del = buf[bufpos];
-	
+		//Variables to do with delay
+		int dsteps = (int)(arguments.delay / arguments.stepsize);	//number of steps in the delay (i.e. number of elements in the buffer)
+		double* buf[dsteps];
+		for (i = 0; i < dsteps; ++i) {
+			buf[i] = (double*) malloc(sizeof(double) * nn);		//allocates memory for an array of arrays, length depending on the number of neurons in the simulation
+		}
+		int bufpos = 0;								//holds the position in the buffer that the del  pointer is at
+		del = buf[bufpos];
+		
 
-	//Variables to do with initial PRC measurements
-	double normalperiod;					//unperturbed period of the oscillation, difference between snd_time and fst_time;
-	int psteps;								//number of steps in the unperturbed period
-	double sptimes[SAMPLESIZE + OFFSET];	//array of times of sp(PRCSKIP) ? ((((double)(k) - (double)(psteps)) - (double)(psteps)) / (double)(psteps)) : ((double)(k) - (double)(psteps))/ (double)(psteps);iking, differences will be averaged to find unperturbed period of oscillation
-	int spikecount = 0;						//holds the location the simulation has reached in sptimes
-	double spdiffs[SAMPLESIZE - 1];			//array of differences in the times of spiking, averaged to find the normalperiod
-	double sumdiffs = 0;					//holds the sum of differences in times of sptimes, used for averaging
-	double periodsd;						//standard deviation of the averaged periods
-	
-	//Allocating memory for the storage arrays, checking if I can, so that I don't run out of memory
-	y = (double**) malloc(sizeof(double*) * (nstep + 1));
-	for (i = 0; i < (nstep + 1); i++) {
-		y[i] = (double*) malloc(sizeof(double) * (N * nn));
-		if (y[i] == NULL) {
-			fprintf(stderr, "Ran out of memory for storage array at y[%d]", i);
+		//Variables to do with initial PRC measurements
+		double normalperiod;					//unperturbed period of the oscillation, difference between snd_time and fst_time;
+		int psteps;								//number of steps in the unperturbed period
+		double sptimes[SAMPLESIZE + OFFSET];	//array of times of sp(PRCSKIP) ? ((((double)(k) - (double)(psteps)) - (double)(psteps)) / (double)(psteps)) : ((double)(k) - (double)(psteps))/ (double)(psteps);iking, differences will be averaged to find unperturbed period of oscillation
+		int spikecount = 0;						//holds the location the simulation has reached in sptimes
+		double spdiffs[SAMPLESIZE - 1];			//array of differences in the times of spiking, averaged to find the normalperiod
+		double sumdiffs = 0;					//holds the sum of differences in times of sptimes, used for averaging
+		double periodsd;						//standard deviation of the averaged periods
+		
+		//Allocating memory for the storage arrays, checking if I can, so that I don't run out of memory
+		y = (double**) malloc(sizeof(double*) * (nstep + 1));
+		for (i = 0; i < (nstep + 1); i++) {
+			y[i] = (double*) malloc(sizeof(double) * (N * nn));
+			if (y[i] == NULL) {
+				fprintf(stderr, "Ran out of memory for storage array at y[%d]", i);
+				return 0;
+			}
+		}
+		xx = (double*) malloc(sizeof(double) * (nstep + 1));
+		if (xx == NULL) {
+			fprintf(stderr, "Ran out of memory for storage array xx]");
+			return 0;
+		} 
+		
+		time = STARTTIME;
+		scan_(v, N*nn, arguments.args[0]);				//scanning in initial variables (state variables only) 
+
+		if (arguments.delay >= arguments.stepsize) {
+			for (i = 0; i < (dsteps); ++i) {//sets every double in buffer(s) to be equal to 0
+				for (j = 0; j < nn; ++j) {
+						buf[i][j] = 0.0;
+				}
+			}
+		}
+
+		
+		iapps[0] = arguments.iapparg;
+		for (i = 1; i < nn; ++i) {						//adding heterogeneity through slightly different iapps like mccarthy does
+			iapps[i] = iapps[i - 1] + 0.005;
+		}
+		
+		derivs(time, v, dv, del, weight);
+		rk4(v, dv, (N * nn), time, arguments.stepsize, vout, del, weight);
+		
+		
+		xx[0] = STARTTIME;		
+		for (i = 0; i < (N * nn); ++i) {
+			v[i] = vout[i]; 
+			y[0][i] = v[i];
+			
+		}
+
+		for (k = 0; k < nstep; ++k) {
+			
+			if (arguments.lowprop && time > arguments.lowstart && time < arguments.lowend) {	//changes gsyn to match the correct level of propofol in the simulation
+				gsyn = (time > arguments.lowstart && time < arguments.lowend) ? LOWPROP_GSYN : G_SYN; 
+				tau  = (time > arguments.lowstart && time < arguments.lowend) ? LOWPROP_TAU : TAUSYN; 
+			}
+			else if (arguments.highprop && time > arguments.highstart && time < arguments.highend) {
+				gsyn = (time > arguments.highstart && time < arguments.highend) ? HIGHPROP_GSYN : G_SYN;
+				tau  = (time > arguments.highstart && time < arguments.highend) ? HIGHPROP_TAU : TAUSYN;
+			}
+			else {	
+				gsyn = (G_SYN);
+				tau  = TAUSYN;
+			}
+			
+			del = buf[bufpos]; //moves the pointer one step ahead in the buffer
+			derivs(time, v, dv, del, weight);										//actually does the step
+			rk4(v, dv, (N * nn), time, arguments.stepsize, vout, del, weight);				//actually does the step
+			
+			if (arguments.delay >= arguments.stepsize) {
+				for (j = 0; j < nn; ++j) {								
+					del[j] = vout[S + (N * j)];							//dereferences the delay pointer, and puts the previous y[S] in the same spot
+				}
+			}
+			if (vout[0] >= THRESHOLD && v[0] < THRESHOLD) {
+				if (spikecount < (SAMPLESIZE + OFFSET)) {
+					if (INTERPOLATE) {
+						sptimes[spikecount] = ((((THRESHOLD) - v[0]) / (vout[0] - v[0])) * (time - (time - arguments.stepsize))) + (time - arguments.stepsize);
+					}
+					else {
+						sptimes[spikecount] = time;
+					}
+				}
+				++spikecount;			//incremented at the end so it can be used as position in sptimes			
+			}
+			
+			time += arguments.stepsize;
+			xx[k + 1] = time;
+			
+			if (arguments.delay >= arguments.stepsize) {
+				bufpos = (++bufpos)%dsteps;
+			}
+			
+			for (i = 0; i < (N * nn); i++) {
+				v[i] = vout[i];
+				y[k + 1][i] = v[i];
+			}
+		}
+		
+		makeallvolts(y, xx, nstep, FULLNAME);
+		
+		if (arguments.verbose) {
+			dump_(vout);
+			makedata(y, xx, nstep, V, "v.data");
+			//makedata(y, xx, nstep, (V + N), "v2.data");
+			makedata(y, xx, nstep, M, "m.data");
+			makedata(y, xx, nstep, H, "h.data");
+			makedata(y, xx, nstep, NV, "n.data");
+			makedata(y, xx, nstep, S, "s.data");
+			makefull(y, xx, nstep, "full.data");
+			
+			
+			//~ makefullsingle(y, xx, nstep, 0, "0full.data");
+			//~ makefullsingle(y, xx, nstep, 1, "1full.data");
+			//~ makefullsingle(y, xx, nstep, 2, "2full.data");
+			//~ makefullsingle(y, xx, nstep, 3, "3full.data");
+			//~ makefullsingle(y, xx, nstep, 4, "4full.data");
+			//~ makefullsingle(y, xx, nstep, 5, "5full.data");
+			//~ makefullsingle(y, xx, nstep, 6, "6full.data");
+			//~ makefullsingle(y, xx, nstep, 7, "7full.data");
+			//~ makefullsingle(y, xx, nstep, 8, "8full.data");
+			//~ makefullsingle(y, xx, nstep, 9, "9full.data");
+			//~ makefullsingle(y, xx, nstep, 10, "10full.data");
+			//~ makefullsingle(y, xx, nstep, 11, "11full.data");
+			//~ makefullsingle(y, xx, nstep, 12, "12full.data");
+			//~ makefullsingle(y, xx, nstep, 13, "13full.data");
+			//~ makefullsingle(y, xx, nstep, 14, "14full.data");
+			//~ makefullsingle(y, xx, nstep, 15, "15full.data");
+			//~ makefullsingle(y, xx, nstep, 16, "16full.data");
+			//~ makefullsingle(y, xx, nstep, 17, "17full.data");
+			//makefullsingle(y, xx, nstep, 18, "18full.data");
+			//~ makefullsingle(y, xx, nstep, 19, "19full.data");
+		}
+		else {
+			fprintf(stderr, "\n\nSince verbose == 0, v-n.data are not being written Only vfull.data\n\n");
+		}
+		fprintf(stderr,"This simulation counted %d spikes of Neuron[0].\n", spikecount);\
+		if (arguments.verbose) {
+					fprintf(stderr, "Printing spike times\n");
+					printdarr(sptimes, spikecount);
+				}
+		if (spikecount >= (SAMPLESIZE + OFFSET)) {
+			for (i = OFFSET; i < SAMPLESIZE + OFFSET - 1; ++i) {		//calculates differences between spike times to find each period
+				sumdiffs += sptimes[i + 1] - sptimes[i];
+				spdiffs[i - OFFSET] = sptimes[i + 1] - sptimes[i];
+			}
+			if (arguments.verbose) {
+				printperiod(spdiffs, SAMPLESIZE - 1, "period.data");
+			}
+			normalperiod = sumdiffs / (SAMPLESIZE - 1);
+			psteps = (int)round(normalperiod / arguments.stepsize);
+			periodsd = calculateSD(spdiffs, SAMPLESIZE - 1);
+			fprintf(stderr, "The average unperturbed period is %f, which is approximately %d steps.\n", normalperiod, psteps);
+			fprintf(stderr, "The standard deviation is %f.\n", periodsd);
+		}
+		else {
+			fprintf(stderr, "There are not enough spikes to account for sample size and offset or something else has gone wrong.\n");
+			fprintf(stderr, "Killing because it hasn't passed the test of spikecount >= (SAMPLESIZE + OFFSET).\n");
+			fprintf(stderr, "v.data-n.data as well as end.data were still written, but no trace or prc processes occured.\n");
 			return 0;
 		}
-	}
-	xx = (double*) malloc(sizeof(double) * (nstep + 1));
-	if (xx == NULL) {
-		fprintf(stderr, "Ran out of memory for storage array xx]");
-		return 0;
-	} 
-	
-	time = STARTTIME;
-	scan_(v, N*nn, arguments.args[0]);				//scanning in initial variables (state variables only) 
 
-	if (arguments.delay >= arguments.stepsize) {
-		for (i = 0; i < (dsteps); ++i) {//sets every double in buffer(s) to be equal to 0
-			for (j = 0; j < nn; ++j) {
-					buf[i][j] = 0.0;
-			}
+		for (i = 0; i < dsteps; i++) {
+			free(buf[i]);
 		}
-	}
+		for (i = 0; i < (nstep + 1); i++) {		
+			free(y[i]);
+		}
+		free(xx);
+		free(iapps);
 
-	
-	iapps[0] = arguments.iapparg;
-	for (i = 1; i < nn; ++i) {						//adding heterogeneity through slightly different iapps like mccarthy does
-		iapps[i] = iapps[i - 1] + 0.005;
 	}
-	
-	derivs(time, v, dv, del, weight);
-	rk4(v, dv, (N * nn), time, arguments.stepsize, vout, del, weight);
-	
-	
-	xx[0] = STARTTIME;		
-	for (i = 0; i < (N * nn); ++i) {
-		v[i] = vout[i]; 
-		y[0][i] = v[i];
-		
-	}
-
-	for (k = 0; k < nstep; ++k) {
-		
-		if (arguments.lowprop && time > arguments.lowstart && time < arguments.lowend) {	//changes gsyn to match the correct level of propofol in the simulation
-			gsyn = (time > arguments.lowstart && time < arguments.lowend) ? LOWPROP_GSYN : G_SYN; 
-			tau  = (time > arguments.lowstart && time < arguments.lowend) ? LOWPROP_TAU : TAUSYN; 
-		}
-		else if (arguments.highprop && time > arguments.highstart && time < arguments.highend) {
-			gsyn = (time > arguments.highstart && time < arguments.highend) ? HIGHPROP_GSYN : G_SYN;
-			tau  = (time > arguments.highstart && time < arguments.highend) ? HIGHPROP_TAU : TAUSYN;
-		}
-		else {	
-			gsyn = (G_SYN);
-			tau  = TAUSYN;
-		}
-		
-		del = buf[bufpos]; //moves the pointer one step ahead in the buffer
-		derivs(time, v, dv, del, weight);										//actually does the step
-		rk4(v, dv, (N * nn), time, arguments.stepsize, vout, del, weight);				//actually does the step
-		
-		if (arguments.delay >= arguments.stepsize) {
-			for (j = 0; j < nn; ++j) {								
-				del[j] = vout[S + (N * j)];							//dereferences the delay pointer, and puts the previous y[S] in the same spot
-			}
-		}
-		if (vout[0] >= THRESHOLD && v[0] < THRESHOLD) {
-			if (spikecount < (SAMPLESIZE + OFFSET)) {
-				if (INTERPOLATE) {
-					sptimes[spikecount] = ((((THRESHOLD) - v[0]) / (vout[0] - v[0])) * (time - (time - arguments.stepsize))) + (time - arguments.stepsize);
-				}
-				else {
-					sptimes[spikecount] = time;
-				}
-			}
-			++spikecount;			//incremented at the end so it can be used as position in sptimes			
-		}
-		
-		time += arguments.stepsize;
-		xx[k + 1] = time;
-		
-		if (arguments.delay >= arguments.stepsize) {
-			bufpos = (++bufpos)%dsteps;
-		}
-		
-		for (i = 0; i < (N * nn); i++) {
-			v[i] = vout[i];
-			y[k + 1][i] = v[i];
-		}
-	}
-	
-	makeallvolts(y, xx, nstep, FULLNAME);
-	
-	if (arguments.verbose) {
-		dump_(vout);
-		makedata(y, xx, nstep, V, "v.data");
-		//makedata(y, xx, nstep, (V + N), "v2.data");
-		makedata(y, xx, nstep, M, "m.data");
-		makedata(y, xx, nstep, H, "h.data");
-		makedata(y, xx, nstep, NV, "n.data");
-		makedata(y, xx, nstep, S, "s.data");
-		makefull(y, xx, nstep, "full.data");
-		
-		
-		//~ makefullsingle(y, xx, nstep, 0, "0full.data");
-		//~ makefullsingle(y, xx, nstep, 1, "1full.data");
-		//~ makefullsingle(y, xx, nstep, 2, "2full.data");
-		//~ makefullsingle(y, xx, nstep, 3, "3full.data");
-		//~ makefullsingle(y, xx, nstep, 4, "4full.data");
-		//~ makefullsingle(y, xx, nstep, 5, "5full.data");
-		//~ makefullsingle(y, xx, nstep, 6, "6full.data");
-		//~ makefullsingle(y, xx, nstep, 7, "7full.data");
-		//~ makefullsingle(y, xx, nstep, 8, "8full.data");
-		//~ makefullsingle(y, xx, nstep, 9, "9full.data");
-		//~ makefullsingle(y, xx, nstep, 10, "10full.data");
-		//~ makefullsingle(y, xx, nstep, 11, "11full.data");
-		//~ makefullsingle(y, xx, nstep, 12, "12full.data");
-		//~ makefullsingle(y, xx, nstep, 13, "13full.data");
-		//~ makefullsingle(y, xx, nstep, 14, "14full.data");
-		//~ makefullsingle(y, xx, nstep, 15, "15full.data");
-		//~ makefullsingle(y, xx, nstep, 16, "16full.data");
-		//~ makefullsingle(y, xx, nstep, 17, "17full.data");
-		//makefullsingle(y, xx, nstep, 18, "18full.data");
-		//~ makefullsingle(y, xx, nstep, 19, "19full.data");
-	}
-	else {
-		fprintf(stderr, "\n\nSince verbose == 0, v-n.data are not being written Only vfull.data\n\n");
-	}
-	fprintf(stderr,"This simulation counted %d spikes of Neuron[0].\n", spikecount);\
-	if (arguments.verbose) {
-				fprintf(stderr, "Printing spike times\n");
-				printdarr(sptimes, spikecount);
-			}
-	if (spikecount >= (SAMPLESIZE + OFFSET)) {
-		for (i = OFFSET; i < SAMPLESIZE + OFFSET - 1; ++i) {		//calculates differences between spike times to find each period
-			sumdiffs += sptimes[i + 1] - sptimes[i];
-			spdiffs[i - OFFSET] = sptimes[i + 1] - sptimes[i];
-		}
-		if (arguments.verbose) {
-			printperiod(spdiffs, SAMPLESIZE - 1, "period.data");
-		}
-		normalperiod = sumdiffs / (SAMPLESIZE - 1);
-		psteps = (int)round(normalperiod / arguments.stepsize);
-		periodsd = calculateSD(spdiffs, SAMPLESIZE - 1);
-		fprintf(stderr, "The average unperturbed period is %f, which is approximately %d steps.\n", normalperiod, psteps);
-		fprintf(stderr, "The standard deviation is %f.\n", periodsd);
-	}
-	else {
-		fprintf(stderr, "There are not enough spikes to account for sample size and offset or something else has gone wrong.\n");
-		fprintf(stderr, "Killing because it hasn't passed the test of spikecount >= (SAMPLESIZE + OFFSET).\n");
-		fprintf(stderr, "v.data-n.data as well as end.data were still written, but no trace or prc processes occured.\n");
-		return 0;
-	}
-
-	for (i = 0; i < dsteps; i++) {
-		free(buf[i]);
-	}
-	for (i = 0; i < (nstep + 1); i++) {		
-		free(y[i]);
-	}
-	free(xx);
-	free(iapps);
 
 	//Starting PRC simulation, if requested
 	if (arguments.prc || arguments.trace) {
